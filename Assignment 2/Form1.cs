e977a4539dfc60a20aee56e249b68f482d874084
jack swanson <RC0032050@CRO-24-04>
2018-10-30 09:54:10 +0000

    Code finished...

--- Assignment 2/Form1.cs
+++ Assignment 2/Form1.cs
@@ -16,5 +16,118 @@ namespace Assignment_2
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double r1, r2, r3, r4, r5;
+
+            try
+            {
+                r1 = double.Parse(textBox6.Text);
+            }
+            catch
+            {
+                r1 = 0;
+            }
+
+            try
+            {
+                r2 = double.Parse(textBox7.Text);
+            }
+            catch
+            {
+                r2 = 0;
+            }
+
+            try
+            {
+                r3 = double.Parse(textBox8.Text);
+            }
+            catch
+            {
+                r3 = 0;
+            }
+
+            try
+            {
+                r4 = double.Parse(textBox9.Text);
+            }
+            catch
+            {
+                r4 = 0;
+            }
+
+            try
+            {
+                r5 = double.Parse(textBox10.Text);
+            }
+            catch
+            {
+                r5 = 0;
+            }
+            double rt = r1 + r2 + r3 + r4 + r5;
+            label2.Text = "Total resistance =" + rt;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double r1, r2, r3, r4, r5;
+
+            double rrt = 0;
+
+            try
+            {
+                r1 = double.Parse(textBox1.Text);
+                rrt = rrt + 1 / r1;
+            }
+            catch
+            {
+
+            }
+            try
+            {
+                r2 = double.Parse(textBox2.Text);
+                rrt = rrt + 1 / r2;
+            }
+            catch
+            {
+
+            }
+            try
+            {
+                r3 = double.Parse(textBox3.Text);
+                rrt = rrt + 1 / r3;
+            }
+            catch
+            {
+
+            }
+            try
+            {
+                r4 = double.Parse(textBox4.Text);
+                rrt = rrt + 1 / r4;
+            }
+            catch
+            {
+
+            }
+            try
+            {
+                r5 = double.Parse(textBox5.Text);
+                rrt = rrt + 1 / r5;
+            }
+            catch
+            {
+
+            }
+            double rt = 1 / rrt;
+            label1.Text = "total resistance =" + rt;
+               
+        }
     }
 }
